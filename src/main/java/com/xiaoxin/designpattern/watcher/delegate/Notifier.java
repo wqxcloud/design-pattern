162e3c87d0f67a50c9bd26a1d9d26529c43b1aa1
zhangyongxin <fyxiaoxin0429@sina.com>
2018-04-24 15:53:34 +0800

    观察者模式-- 方法委托

--- src/main/java/com/xiaoxin/designpattern/watcher/delegate/Notifier.java
+++ src/main/java/com/xiaoxin/designpattern/watcher/delegate/Notifier.java
@@ -18,8 +18,7 @@ public abstract class Notifier {
      * @param methodName 执行方法 的方法名
      * @param args   执行方法的参数
      */
-    public abstract void addListener(Object object, String methodName,
-            Object... args);
+    public abstract void addListener(Object object, String methodName, Object... args);
 
     /**
      * 告诉所有要帮忙放哨的学生：老师来了
