26fe0cb993f0d26292fe933a40f8553ea0906e7b
zhangyongxin <fyxiaoxin0429@sina.com>
2018-04-25 23:17:13 +0800

    增加对比

--- src/main/java/com/xiaoxin/designpattern/state/TestMain.java
+++ src/main/java/com/xiaoxin/designpattern/state/TestMain.java
@@ -13,14 +13,37 @@ public class TestMain {
      * 总结：状态模式适合 多if-else分支的判断方法进行优化的场景使用。这样可以消除庞大的条件分支语句
      * 当一个对象的行为取决于它的状态，并且它必须在运行时根据状态改变它的行为是，就可以使用状态模式了。
      * 不过需要注意的是，状态逻辑中的条件判断避免写成死循环。
+     * testState 方法的效果等价于 testCondition
      */
     @Test
-    public void test(){
+    public void testState(){
         Citizen citizen = new Citizen(AreaTypeEnum.COUNTY.getType());
         citizen.living();
         citizen.setType(AreaTypeEnum.CITY.getType());
         citizen.living();
         citizen.setType(AreaTypeEnum.PROVINCE.getType());
         citizen.living();
+    }
+
+    @Test
+    public void testCondition(){
+        Citizen citizen = new Citizen(AreaTypeEnum.COUNTY.getType());
+        living(citizen);
+        citizen.setType(AreaTypeEnum.CITY.getType());
+        living(citizen);
+        citizen.setType(AreaTypeEnum.PROVINCE.getType());
+        living(citizen);
+    }
+
+    private void living(Citizen citizen) {
+        AreaLiving living = new CountyAreaLiving(citizen);
+        if (citizen.getType() == AreaTypeEnum.COUNTY.getType()) {
+            living = new CountyAreaLiving(citizen);
+        } else if (citizen.getType() == AreaTypeEnum.CITY.getType()) {
+            living = new CityAreaLiving(citizen);
+        } else if (citizen.getType() == AreaTypeEnum.PROVINCE.getType()) {
+            living = new ProvinceAreaLiving(citizen);
+        }
+        living.whatLife();
     }
 }
