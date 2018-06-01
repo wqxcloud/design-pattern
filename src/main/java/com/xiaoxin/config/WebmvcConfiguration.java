985a42ae7f00f275b0ad282c182dc1f426201af8
zhangyongxin <fyxiaoxin0429@sina.com>
2018-06-01 17:08:03 +0800

    异常处理返回

--- src/main/java/com/xiaoxin/config/WebmvcConfiguration.java
+++ src/main/java/com/xiaoxin/config/WebmvcConfiguration.java
@@ -5,20 +5,18 @@ import com.xiaoxin.validator.model.Result;
 import com.xiaoxin.validator.model.ResultGenerator;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.method.HandlerMethod;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.mvc.method.annotation.ExceptionHandlerExceptionResolver;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
 /**
- *
  * 所有的内容协商器、拦截器、统一异常处理、messageResolver、跨域配置等都可以在这里配置
+ *
  * @Auther zhangyongxin
  * @date 2018/5/30 下午4:56
  */
@@ -32,9 +30,13 @@ public class WebmvcConfiguration implements WebMvcConfigurer {
             Result result = ResultGenerator.genFailResult(exception.getMessage());
             try {
                 httpServletResponse.getWriter().write(new Gson().toJson(result));
+                httpServletResponse.setCharacterEncoding("UTF-8");
+                httpServletResponse.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON.toString());
+                httpServletResponse.setStatus(200);
             } catch (IOException e) {
                 e.printStackTrace();
             }
+            log.error("there is an Error happened {}", exception);
             return new ModelAndView();
         });
     }
