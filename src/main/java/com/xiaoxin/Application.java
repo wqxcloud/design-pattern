04ded44d319eb0905045df0ed461b51662a5aa5c
zhangyongxin <fyxiaoxin0429@sina.com>
2018-06-01 17:30:30 +0800

    解决mybatis DataSource初始化失败的问题

--- src/main/java/com/xiaoxin/Application.java
+++ src/main/java/com/xiaoxin/Application.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan(basePackages = {"com.xiaoxin"})
 @SpringBootApplication
 @EnableCaching
-@EnableAutoConfiguration
 public class Application {
 
     public static void main(String[] args) {
--- src/main/java/com/xiaoxin/config/DataSourceConfig.java
+++ src/main/java/com/xiaoxin/config/DataSourceConfig.java
@@ -1,12 +1,24 @@
 package com.xiaoxin.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
 
 /**
  * @Auther zhangyongxin
  * @date 2018/5/19 下午12:17
  */
 @Configuration
+@ConfigurationProperties("spring.datasource")
 public class DataSourceConfig extends DruidDataSource{
+
+    @Bean
+    @Primary
+    public DataSource dataSource(){
+        return this;
+    }
 }
